2a4110f971782b9786d410dc4b376c4a8f96ad2b
Citlalli Montes <citlalimontes95@gmail.com>
2019-09-25 19:44:12 -0500

    usando estilos css

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Bienvenido a Automart</h1>')
+     return render(request, 'index.html', {'autos': autos})
+
+class Auto:
+    def __init__(self, nombre, precio, modelo, color):
+        self.nombre = nombre
+        self.precio = precio 
+        self.modelo = modelo 
+        self.color = color
+
+
+autos = [
+    Auto('Jetta',145000, 2018,"Gris"),
+    Auto('Camaro', 600000, 2019, ' Negro'),
+    Auto('Audi 7', 90000, 2018, 'blanco'),
+    Auto('Futura', 900000, 1954, 'Aqua'),
+]
